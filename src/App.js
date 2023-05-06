33711e8f92105a6ed3a545d067ced37049e17093
tabasshinafrinmeghla <tabasshinafrinmeghla@gmail.com>
2023-05-06 16:09:04 +0600

    add all

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-const products =[
-  {name:'laptop', price : 150000},
-  {name:'Phone', price : 50000},
-  {name:'Watch', price : 1500},
-  {name:'tablet', price : 1500},
+import { useState } from 'react';
+// const products =[
+//   {name:'laptop', price : 150000},
+//   {name:'Phone', price : 50000},
+//   {name:'Watch', price : 1500},
+//   {name:'tablet', price : 1500},
   
-]
-const books=[
-  {name:'math',price:300},
-  {name:'ban',price:200},
-  {name:'eng',price:100},
-  {name:'bio',price:400},
-  {name:'phy',price:700},
-  {name:'chy',price:600},
-  {name:'riligious',price:250},
-  {name:'drowing',price:340},
-]
+// ]
+// const books=[
+//   {name:'math',price:300},
+//   {name:'ban',price:200},
+//   {name:'eng',price:100},
+//   {name:'bio',price:400},
+//   {name:'phy',price:700},
+//   {name:'chy',price:600},
+//   {name:'riligious',price:250},
+//   {name:'drowing',price:340},
+// ]
 
 
 
 function App() {
   return (
     <div className="App">
-      {
+      {/* {
         products.map(products=> <Product name={products.name} price={products.price}></Product>)
       }
 
@@ -31,19 +32,56 @@ function App() {
         books.map(books=> <Product name={books.name} price={books.price}></Product>)
       }
       <Product name="laptop" price="100000"></Product>
-      <Product name="phone" price="50000"></Product>
+      <Product name="phone" price="50000"></Product> */
+      
+      }
+
+      <Counter></Counter>
     </div>
-  );
+   );
 }
+
+
+
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  console.log(count);
+
+  const increaseCount = () =>setCount(count + 1);
+  
+  
+
+  // simple vabe korte =>..........
+  // const increaseCount = () =>{
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // }
+  // console.log(abc);
+  return(
+    <div>
+      <h1>count: {count}</h1>
+      <button onClick={increaseCount} >Increase</button>
+    </div>
+  )
+}
+
+
 
 function Product(props){
   return(
     <div className='Product'>
-      <h3>Name:{props.name} </h3>
-      <p>Price: {props.price} </p>
+      {/* <h3>Name:{props.name} </h3>
+      <p>Price: {props.price} </p> */
+      
+      
+      
+      }
     </div>
   )
 }
+
+
 
 
 
